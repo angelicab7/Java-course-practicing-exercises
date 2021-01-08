ee325d7c2c6ee47c560b4d2f28e31b2e09548175
Angelica Bonilla Riaño <angelicabonilla723@gmail.com>
2021-01-08 10:31:29 -0500

    zoo animals list

--- ZooAnimalsList/src/com/angelica/exercise/Animal.java
+++ ZooAnimalsList/src/com/angelica/exercise/Animal.java
@@ -1,12 +1,14 @@
 package com.angelica.exercise;
 
+import java.util.List;
+
 public class Animal {
     private String name;
     private String specie;
-    private String features;
+    private List<String> features;
     private String mostLikedFeature;
 
-    public Animal (String name, String specie, String features, String mostLikedFeature){
+    public Animal (String name, String specie, List<String> features, String mostLikedFeature){
         this.name = name;
         this.specie = specie;
         this.features = features;
@@ -20,9 +22,11 @@ public class Animal {
     public String getSpecie() {
         return specie;
     }
-    public String getFeatures(){
+
+    public List<String> getFeatures() {
         return features;
     }
+
     public String getMostLikedFeature(){
         return mostLikedFeature;
     }
@@ -33,9 +37,11 @@ public class Animal {
     public void setSpecie(String specie){
         this.specie = specie;
     }
-    public void setFeatures(String features){
+
+    public void setFeatures(List<String> features) {
         this.features = features;
     }
+
     public void  setMostLikedFeature(String mostLikedFeature){
         this.mostLikedFeature = mostLikedFeature;
     }
--- ZooAnimalsList/src/com/angelica/exercise/Main.java
+++ ZooAnimalsList/src/com/angelica/exercise/Main.java
@@ -1,8 +1,45 @@
 package com.angelica.exercise;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner scan = new Scanner(System.in);
+
+        List<Animal> animalList = new ArrayList<Animal>();
+
+        List<String> animalFeatures = new ArrayList<>();
+        animalFeatures.add("Grey");
+        animalFeatures.add("Big");
+        animalList.add(new Animal("Elephant", "Mammal", animalFeatures, "Horns"));
+
+        animalFeatures = new ArrayList<>();
+        animalFeatures.add("Yellow");
+        animalFeatures.add("King");
+        animalList.add(new Animal("Lion", "Mammal", animalFeatures, "Jungle King"));
+
+        animalFeatures = new ArrayList<>();
+        animalFeatures.add("Black");
+        animalFeatures.add("Dangerous");
+        animalFeatures.add("Poisonous");
+        animalList.add(new Animal("Snake", "Reptile", animalFeatures, "Fangs"));
+
+        animalFeatures = new ArrayList<>();
+        animalFeatures.add("Chubby");
+        animalFeatures.add("Clumsy");
+        animalList.add(new Animal("Panda bear", "Mammal", animalFeatures, "Cute"));
+
+        for(int i = 0; i < animalList.size(); i ++){
+            Animal currentAnimal = animalList.get(i);
+            System.out.println("Name: " + currentAnimal.getName());
+            System.out.println("Specie: " + currentAnimal.getSpecie());
+            System.out.println("Features: " + currentAnimal.getFeatures());
+            System.out.println("Most liked feature: " + currentAnimal.getMostLikedFeature() + "\n");
+
+        }
     }
+
 }
