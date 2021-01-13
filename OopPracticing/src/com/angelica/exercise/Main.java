b5b30492baaf23a856bedf34e6a38a2a97411d74
Angelica Bonilla Riaño <angelicabonilla723@gmail.com>
2021-01-13 15:58:52 -0500

    new changes!

---
+++ OopPracticing/src/com/angelica/exercise/Main.java
@@ -0,0 +1,10 @@
+package com.angelica.exercise;
+
+public class Main {
+
+    public static void main(String[] args) { //object
+	  Person firstPerson;
+	  firstPerson = new Person();
+	  
+    }
+}
---
+++ OopPracticing/src/com/angelica/exercise/Person.java
@@ -0,0 +1,11 @@
+package com.angelica.exercise;
+
+public class Person {
+     private String name;  //Attribute
+     private String lastName;
+
+   public void getInfo(){ //Method
+       System.out.println("Name:" + name);
+       System.out.println("Last name:" + lastName);
+   }
+}
