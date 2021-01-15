631e561b181a735375a6bd99353258abc2d61760
Angelica Bonilla Riaño <angelicabonilla723@gmail.com>
2021-01-15 09:11:51 -0500

    box volume done!

--- BoxProject/src/com/angelica/exercise/Main.java
+++ BoxProject/src/com/angelica/exercise/Main.java
@@ -7,7 +7,18 @@ public class Main {
         int boxHeight = 2;
         int boxDepth = 6;
 
-        Box firstBox = new Box(boxWidth, boxHeight, boxDepth);
+        Box firstBox = new Box(boxWidth, boxHeight, boxDepth);{
+            firstBox.boxWidth = boxWidth;
+            firstBox.boxHeight =boxHeight;
+            firstBox.boxDepth = boxDepth;
+            int result = firstBox.calculateBoxVolume();
+            System.out.println("The first box´s measure is: " + result);
+
+        Box secondBox = new Box(2,4,6);
+            System.out.println("The second box´s measure is: " + secondBox.calculateBoxVolume());
+
+
+        }
     }
 
 }
